81790a2e8c9d4655bfe917ebadf551d89a6145b8
Michael Ambrus <michael.ambrus@gmail.com>
2012-11-19 20:46:00 +0100

    [grit]: mline.sh parameters
    grit.mline.sh can now print specific parameters and enables certain
    formatting.
    
    Category: development

--- ui/.pline.sh
+++ ui/.pline.sh
@@ -4,7 +4,7 @@
 
 function print_pline_help() {
 			cat <<EOF
-Usage: $PLINE_SH_INFO [options] -- </regexp_pattern/><linenumber> [</regexp_pattern/><linenumber>]
+Usage: $PLINE_SH_INFO [options] -- </regexp/><line-num> [</regexp/><line-num>]
 
 Cut out a section of text from a file. It's basically sed, but with a better
 UI. Normally section to cut are given as line-numbers, but instead of
@@ -12,20 +12,25 @@ linenumbers, text-markers can be given. If so, they have to be given in regexp
 form and they have to be surrounded by '/'.
 
 Script can take one or two arguments and can take input either from file or
-stdin. Stdin is ment to be piped, no console stdin i possible.
+stdin. Stdin input is ment to be piped.
 
 Options:
   -f <filename>		Read input from this file instead of from stdin
   -h				This help
 
 Example:
-  $PLINE_<S-F12>SH_INFO
+  $PLINE_SH_INFO -f text.txt 10 20
+  #Return lines 10..20 from file text.txt
+  
+  $PLINE_SH_INFO '/<manifest>/' '/\/<manifest>/' < manifest.xml
+  #Plint only the manifest content from a repo manifest file
 
 EOF
 }
 	while getopts f:h OPTION; do
 		case $OPTION in
 		h)
+			clear
 			print_pline_help $0
 			exit 0
 			;;
